a85842204d41402acf289c606a5f2bab369224b1
Niran Babalola <niran@niran.org>
2011-07-26 18:57:44 -0500

    Replace publication cache with a memoized default method

--- armstrong/core/arm_publication/models.py
+++ armstrong/core/arm_publication/models.py
@@ -3,11 +3,11 @@ import datetime
 from django.conf import settings
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey
 
-PUBLICATION_CACHE = {}
 
 class PublicationManager(models.Manager):
     """
@@ -18,27 +18,34 @@ class PublicationManager(models.Manager):
     """
 
     def get_default(self):
+        """Returns the default Publication.
+
+        Either the sole Publication or the Publication referenced by
+        The default Publication is either the one specified by
+        ARMSTRONG_DEFAULT_PUBLICATION_ID or the only Publication in the
+        database. If neither situation applies, an error will be raised.
+        """
         try:
-            publication_id = settings.DEFAULT_PUBLICATION_ID
+            publication_id = settings.ARMSTRONG_DEFAULT_PUBLICATION_ID
+            return self.get(pk=publication_id)
         except AttributeError:
-            from django.core.exceptions import ImproperlyConfigured
-            raise ImproperlyConfigured(
-                "The DEFAULT_PUBLICATION_ID setting must be provided " + \
-                "when using the publications framework.")
-        try:
-            default_publication = PUBLICATION_CACHE[publication_id]
-        except KeyError:
-            default_publication = self.get(pk=publication_id)
-            PUBLICATION_CACHE[publication_id] = default_publication
-        return default_publication
+            pass
+
+        if Publication.objects.count() == 1:
+            return Publication.objects.all()[0]
+
+        if Publication.objects.count() == 0:
+            raise Publication.DoesNotExist
+
+        raise ImproperlyConfigured(
+            "The ARMSTRONG_DEFAULT_PUBLICATION_ID setting must be provided "
+            "when using the publications framework.")
 
     @property
     def default(self):
-        return self.get_default()
-
-    def clear_cache(self):
-        global PUBLICATION_CACHE
-        PUBLICATION_CACHE = {}
+        if not hasattr(self, '_default'):
+            self._default = self.get_default()
+        return self._default
 
 
 class Publication(MPTTModel):
@@ -58,19 +65,6 @@ class Publication(MPTTModel):
 
     def __unicode__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        super(Publication, self).save(*args, **kwargs)
-        if self.id in PUBLICATION_CACHE:
-            del PUBLICATION_CACHE[self.id]
-
-    def delete(self):
-        pk = self.pk
-        super(Publication, self).delete()
-        try:
-            del PUBLICATION_CACHE[pk]
-        except KeyError:
-            pass
 
 
 class PublicationNode(models.Model):
